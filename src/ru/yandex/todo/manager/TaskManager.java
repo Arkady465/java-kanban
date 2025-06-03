65a85f05acd15f069d7b0287a797444ea2fb14f3
Arkady465 <155990363+Arkady465@users.noreply.github.com>
2025-06-03 14:34:14 +0300

    Add files via upload

--- src/ru/yandex/todo/manager/TaskManager.java
+++ src/ru/yandex/todo/manager/TaskManager.java
@@ -11,29 +11,51 @@ import java.util.Set;
  * Интерфейс менеджера задач.
  */
 public interface TaskManager {
+
     // ===== Методы для Task =====
+
     List<Task> getTasks();
+
     Task getTaskById(int id);
+
     Task createTask(Task task) throws ManagerSaveException;
+
     Task updateTask(Task task) throws ManagerSaveException;
+
     void deleteTask(int id);
 
+
     // ===== Методы для Subtask =====
+
     List<Subtask> getSubtasks();
+
     Subtask getSubtaskById(int id);
+
     Subtask createSubtask(Subtask subtask) throws ManagerSaveException;
+
     Subtask updateSubtask(Subtask subtask) throws ManagerSaveException;
+
     void deleteSubtask(int id);
 
+
     // ===== Методы для Epic =====
+
     List<Epic> getEpics();
+
     Epic getEpicById(int id);
+
     Epic createEpic(Epic epic) throws ManagerSaveException;
+
     Epic updateEpic(Epic epic) throws ManagerSaveException;
+
     void deleteEpic(int id);
+
     List<Subtask> getEpicSubtasks(int epicId);
 
+
     // ===== History & Prioritized =====
+
     List<Task> getHistory();
+
     Set<Task> getPrioritizedTasks();
 }
