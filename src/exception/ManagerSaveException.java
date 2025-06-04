6c00b16b2a36f46b073101bad9272f9f74fdf796
Arkady465 <155990363+Arkady465@users.noreply.github.com>
2025-06-04 22:12:46 +0300

    Add files via upload

---
+++ src/exception/ManagerSaveException.java
@@ -0,0 +1,10 @@
+package exception;
+
+/**
+ * Исключение, возникающее при ошибках сохранения/загрузки из файла.
+ */
+public class ManagerSaveException extends RuntimeException {
+    public ManagerSaveException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
