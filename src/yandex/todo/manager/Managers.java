a6cfb3795c01c101722f7dc869fa5858090a4174
Arkady465 <155990363+Arkady465@users.noreply.github.com>
2025-06-04 20:13:59 +0300

    Add files via upload

--- src/yandex/todo/manager/Managers.java
+++ src/yandex/todo/manager/Managers.java
@@ -1,17 +1,7 @@
-package todo.manager;
+package ru.yandex.todo.manager;
 
-import service.FileBackedTaskManager;
-
-import java.io.File;
-
-/**
- * Утилитный класс – фабрика менеджеров.
- * Метод getDefault() возвращает экземпляр TaskManager
- * (по умолчанию – файловый).
- */
 public class Managers {
     public static TaskManager getDefault() {
-        File file = new File("tasks_data.csv");
-        return new FileBackedTaskManager(file);
+        return new InMemoryTaskManager(); // гарантированно рабочий менеджер
     }
 }
