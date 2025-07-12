03e19aa31c8af150c0f2db406496fa21d5a06a62
Arkady465 <155990363+Arkady465@users.noreply.github.com>
2025-07-12 15:52:45 +0300

    Update PrioritizedHandler.java

--- src/server/PrioritizedHandler.java
+++ src/server/PrioritizedHandler.java
@@ -1,12 +1,10 @@
 package server;
 
 import com.sun.net.httpserver.HttpExchange;
-import com.sun.net.httpserver.HttpHandler;
 import managers.TaskManager;
-
 import java.io.IOException;
 
-public class PrioritizedHandler extends BaseHttpHandler implements HttpHandler {
+public class PrioritizedHandler extends BaseHttpHandler {
     private final TaskManager manager;
 
     public PrioritizedHandler(TaskManager manager) {
@@ -15,12 +13,10 @@ public class PrioritizedHandler extends BaseHttpHandler implements HttpHandler {
 
     @Override
     public void handle(HttpExchange exchange) throws IOException {
-        String method = exchange.getRequestMethod();
-        if (method.equals("GET")) {
+        if ("GET".equals(exchange.getRequestMethod())) {
             sendText(exchange, gson.toJson(manager.getPrioritizedTasks()), STATUS_OK);
         } else {
             sendText(exchange, "Метод не поддерживается", STATUS_METHOD_NOT_FOUND);
         }
-        exchange.close();
     }
 }
