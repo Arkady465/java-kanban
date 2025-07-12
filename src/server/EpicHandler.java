70eca33f5b89f9dc6c6d4712db8eea6c19dfe220
Arkady465 <155990363+Arkady465@users.noreply.github.com>
2025-07-12 16:09:30 +0300

    Update EpicHandler.java

--- src/server/EpicHandler.java
+++ src/server/EpicHandler.java
@@ -20,39 +20,43 @@ public class EpicHandler extends BaseHttpHandler {
     public void handle(HttpExchange exchange) throws IOException {
         String method = exchange.getRequestMethod();
         switch (method) {
-            case "GET":
+            case "GET": {
                 Optional<Integer> idOpt = parseId(exchange.getRequestURI().getQuery());
                 if (idOpt.isPresent()) {
-                    Epic epic = (Epic) manager.getEpic(idOpt.get());
+                    Epic epic = manager.getEpicById(idOpt.get());
                     sendText(exchange, gson.toJson(epic), STATUS_OK);
                 } else {
-                    sendText(exchange, gson.toJson(manager.getEpics()), STATUS_OK);
+                    sendText(exchange, gson.toJson(manager.getAllEpics()), STATUS_OK);
                 }
                 break;
-            case "POST":
+            }
+            case "POST": {
                 InputStream is = exchange.getRequestBody();
                 String body = new String(is.readAllBytes(), StandardCharsets.UTF_8);
                 Epic e = gson.fromJson(body, Epic.class);
                 if (e.getId() == 0) {
-                    manager.createEpic(e);
+                    manager.addEpic(e);
                     sendText(exchange, gson.toJson(e), STATUS_CREATED);
                 } else {
-                    manager.updateEpic(e);
+                    Epic old = manager.getEpicById(e.getId());
+                    manager.updateEpic(old, e);
                     sendText(exchange, gson.toJson(e), STATUS_OK);
                 }
                 break;
-            case "DELETE":
+            }
+            case "DELETE": {
                 Optional<Integer> delId = parseId(exchange.getRequestURI().getQuery());
                 if (delId.isPresent()) {
-                    manager.deleteEpic(delId.get());
+                    Epic old = manager.getEpicById(delId.get());
+                    manager.deleteEpicById(old);
                 } else {
                     manager.deleteAllEpics();
                 }
                 sendText(exchange, "", STATUS_OK);
                 break;
+            }
             default:
                 sendText(exchange, "Метод не поддерживается", STATUS_METHOD_NOT_FOUND);
         }
     }
 }
-
