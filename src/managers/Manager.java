929b175846b8e032b090814e9bf2f5f280e351f5
Arkady465 <155990363+Arkady465@users.noreply.github.com>
2025-07-12 15:33:01 +0300

    Update Manager.java

--- src/managers/Manager.java
+++ src/managers/Manager.java
@@ -1,20 +1,22 @@
 package managers;
 
-import java.io.File;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import java.time.Duration;
+import java.time.LocalDateTime;
+import server.adapter.DurationTypeAdapter;
+import server.adapter.LocalDateTimeTypeAdapter;
 
-public class Manager {
-    public static TaskManager getDefault() {
-        File directory = new File("./data");
-        if (!directory.exists()) {
-            directory.mkdirs();
-        }
+public class Managers {
+    // HTTP-константы
+    public static final String HEADER_CONTENT_TYPE = "Content-Type";
+    public static final String MIME_JSON_UTF8     = "application/json;charset=utf-8";
 
-        File file = new File(directory, "tasks.csv");
-        return FileBackedTaskManager.loadFromFile(getDefaultHistory(), file);
+    /** Возвращает настроенный экземпляр Gson с нужными адаптерами */
+    public static Gson createGson() {
+        return new GsonBuilder()
+            .registerTypeAdapter(Duration.class, new DurationTypeAdapter())
+            .registerTypeAdapter(LocalDateTime.class, new LocalDateTimeTypeAdapter())
+            .create();
     }
-
-    public static HistoryManager getDefaultHistory() {
-        return new InMemoryHistoryManager();
-    }
-
 }
