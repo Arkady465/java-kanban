81dbda0c538e0400fa2e3d43f9b468ae5586fa2b
Arkady465 <155990363+Arkady465@users.noreply.github.com>
2025-07-14 23:18:04 +0300

    Add files via upload

---
+++ server/PrioritizedHandler.java
@@ -0,0 +1,24 @@
+package server;
+
+import com.sun.net.httpserver.HttpExchange;
+import managers.TaskManager;
+
+import java.io.IOException;
+
+public class PrioritizedHandler extends BaseHttpHandler {
+    private final TaskManager manager;
+
+    public PrioritizedHandler(TaskManager manager) {
+        this.manager = manager;
+    }
+
+    @Override
+    public void handle(HttpExchange exchange) throws IOException {
+        String method = exchange.getRequestMethod();
+        if (method.equals("GET")) {
+            sendText(exchange, gson.toJson(manager.getPrioritizedTasks()), STATUS_OK);
+        } else {
+            sendText(exchange, "Метод не поддерживается", STATUS_METHOD_NOT_FOUND);
+        }
+    }
+}
